cfaab35c618a660c9d9456dfa648737911d89b2c
Backstage Admin <admin@backstage.local>
2026-02-10 01:45:21 +0000

    fix(catalog): enable github module in backend to process locations

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -36,6 +36,7 @@ backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
 backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
